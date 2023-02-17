3edd29b32797e3c652d3b4b5274638dfaaf37cba
Scarlette Fox <lexidahusky.102@gmail.com>
2023-02-17 13:02:11 +0000

    [Change] Added prisma and kazagumo to the container

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,54 @@
 import './lib/setup';
 import '@sapphire/plugin-hmr/register';
 
+import { Kazagumo, Plugins } from 'kazagumo';
+import { Connectors } from 'shoukaku';
 import { MiyuClient } from './lib/structures/MiyuClient';
-// import type { SapphireClient } from '@sapphire/framework';
-// import { ClusterClient } from 'discord-hybrid-sharding';
+import spotifyPlugin from 'kazagumo-spotify';
+import { container } from '@sapphire/framework';
+
+const Nodes = [
+	{
+		name: 'main',
+		url: '192.168.1.127:2333',
+		auth: 'rawrx3-Fox',
+		secure: false
+	}
+];
 
 const client = new MiyuClient();
-// as SapphireClient & { cluster: ClusterClient<SapphireClient> };
-// client.cluster = new ClusterClient<SapphireClient>(client);
-console.log(client);
+const kazagumo = new Kazagumo(
+	{
+		defaultSearchEngine: 'soundcloud',
+		plugins: [
+			new Plugins.PlayerMoved(client),
+			new spotifyPlugin({
+				clientId: process.env.SPOTIFY_CLIENT_ID as string,
+				clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
+				playlistPageLimit: 1,
+				albumPageLimit: 1,
+				searchLimit: 10,
+				searchMarket: 'US'
+			})
+		],
+
+		send: (guildId, payload) => {
+			const guild = client.guilds.cache.get(guildId);
+			if (guild) guild.shard.send(payload);
+		}
+	},
+	new Connectors.DiscordJS(client),
+	Nodes,
+	{
+		resume: true
+	}
+);
 
 const main = async () => {
 	try {
+		container.kazagumo = kazagumo;
 		client.logger.info('Logging in');
-		await client.login(process.env.TOKEN);
+		await client.login(process.env.TOKEN as string);
 		client.logger.info('logged in');
 	} catch (error) {
 		client.logger.fatal(error);
--- src/lib/structures/MiyuClient.ts
+++ src/lib/structures/MiyuClient.ts
@@ -1,10 +1,10 @@
-import { LogLevel, SapphireClient } from '@sapphire/framework';
+import { PrismaClient } from '@prisma/client';
+import { container, LogLevel, SapphireClient } from '@sapphire/framework';
 import { ClusterClient, getInfo } from 'discord-hybrid-sharding';
+import type { Kazagumo } from 'kazagumo';
 import { GatewayIntentBits, Partials } from 'discord.js';
 
 export class MiyuClient extends SapphireClient {
-	// public cluster: ClusterClient<SapphireClient>;
-
 	public constructor() {
 		super({
 			defaultPrefix: '+',
@@ -43,10 +43,28 @@ export class MiyuClient extends SapphireClient {
 		});
 		this.cluster = new ClusterClient<SapphireClient>(this);
 	}
+
+	public override async login(token: string) {
+		container.db = new PrismaClient();
+		await container.db.$connect().then(() => container.logger.info('Connected to database'));
+		return super.login(token);
+	}
+
+	public override async destroy() {
+		await container.db.$disconnect();
+		return super.destroy();
+	}
 }
 
 declare module '@sapphire/framework' {
 	interface SapphireClient {
 		cluster: ClusterClient<SapphireClient>;
+	}
+}
+
+declare module '@sapphire/pieces' {
+	interface Container {
+		db: PrismaClient;
+		kazagumo: Kazagumo;
 	}
 }
