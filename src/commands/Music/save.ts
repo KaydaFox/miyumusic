b74635e8bb330d057b376095f7d231e4a4849046
Scarlette Fox <lexidahusky.102@gmail.com>
2023-02-18 15:41:05 +0000

    [Add] Save and shuffle commands

---
+++ src/commands/Music/save.ts
@@ -0,0 +1,60 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+import { EmbedBuilder } from 'discord.js';
+import type { MiyuCommand } from '../../lib/structures/Command';
+
+@ApplyOptions<MiyuCommand.Options>({
+	description: 'Sends the current song to your DMs',
+	fullCategory: ['Music'],
+	usage: '+save',
+	examples: ['+save'],
+	preconditions: ['GuildOnly']
+})
+export class SaveCommand extends Command {
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand((builder) => {
+			builder.setName(this.name).setDescription(this.description);
+		});
+	}
+
+	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		return this.save(interaction);
+	}
+
+	private async save(interaction: Command.ChatInputCommandInteraction) {
+		const { kazagumo } = this.container;
+		const player = kazagumo.players.get(interaction.guildId as string) ?? null;
+
+		if (!player || !player.queue || !player.queue.current) return interaction.reply({ content: 'I am not playing anything!', ephemeral: true });
+
+		const song = player.queue.current;
+		if (!song) return interaction.reply({ content: 'I am not playing anything!', ephemeral: true });
+
+		try {
+			interaction.user.send({
+				embeds: [
+					new EmbedBuilder()
+						.setAuthor({ name: interaction.user.tag, iconURL: interaction.user.avatarURL() ?? undefined })
+						.setTitle(`Saved Song`)
+						.setURL(song.uri)
+						.setThumbnail(song.thumbnail || null)
+						.setDescription(`${song.title} by ${song.author}\n\nRequested by ${song.requester}`)
+						.setColor(this.container.embedColor)
+						.setTimestamp()
+				]
+			});
+
+			return interaction.reply({
+				ephemeral: true,
+				content: `Sent the current song to your DMs!`
+			});
+		} catch (error) {
+			return interaction.reply({
+				ephemeral: true,
+				content: `I couldn't send the current song to your DMs! Please make sure they are open. Otherwise you can click [here](${
+					song.realUri || song.uri
+				}) to open the song in your browser.`
+			});
+		}
+	}
+}
---
+++ src/commands/Music/shuffle.ts
@@ -0,0 +1,52 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+import { EmbedBuilder } from 'discord.js';
+import type { MiyuCommand } from '../../lib/structures/Command';
+
+@ApplyOptions<MiyuCommand.Options>({
+	description: 'Shuffle the queue',
+	fullCategory: ['Music'],
+	usage: '+shuffle',
+	examples: ['+shuffle'],
+	preconditions: ['GuildOnly']
+})
+export class ShuffleCommand extends Command {
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand(
+			(builder) => {
+				builder.setName(this.name).setDescription(this.description);
+			},
+			{
+				idHints: ['1076525573641359360']
+			}
+		);
+	}
+
+	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		return this.shuffle(interaction);
+	}
+
+	private async shuffle(interaction: Command.ChatInputCommandInteraction) {
+		const { kazagumo } = this.container;
+		const player = kazagumo.players.get(interaction.guildId as string) ?? null;
+
+		if (!player) return interaction.reply({ content: 'I am not playing anything!', ephemeral: true });
+
+		const queue = player.queue;
+		if (!queue || !queue.length) return interaction.reply({ content: 'I am not playing anything!', ephemeral: true });
+
+		if (queue.length < 2) return interaction.reply({ content: 'Not enough songs in the queue', ephemeral: true });
+
+		await queue.shuffle();
+
+		return interaction.reply({
+			embeds: [
+				new EmbedBuilder()
+					.setAuthor({ name: interaction.user.tag, iconURL: interaction.user.avatarURL() ?? undefined })
+					.setDescription(`Shuffled the queue`)
+					.setColor(this.container.embedColor)
+					.setTimestamp()
+			]
+		});
+	}
+}
